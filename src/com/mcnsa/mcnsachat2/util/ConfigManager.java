13df065e8db28893e77529725412887be8d91127
FuzzyWuzzie <kenton@hamaluik.com>
2012-02-09 12:09:25 -0700

    Added skeleton save function

--- src/com/mcnsa/mcnsachat2/util/ConfigManager.java
+++ src/com/mcnsa/mcnsachat2/util/ConfigManager.java
@@ -3,6 +3,11 @@ package com.mcnsa.mcnsachat2.util;
 import com.mcnsa.mcnsachat2.MCNSAChat2;
 
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ConfigManager {
 	// store the main plugin for later access
@@ -10,9 +15,59 @@ public class ConfigManager {
 	public ConfigManager(MCNSAChat2 instance) {
 		plugin = instance;
 	}
+
+	public Boolean configExists(String file) {
+		// see if it exists by trying to open it
+		File configFile = new File(file);
+		Boolean exists = configFile.exists();
+		// close up
+		configFile.close();
+		return exists;
+	}
 	
-	public void load(String file) {
+	public Boolean load() {
+		// make sure the config exists before we try to load it!
+		if(!configExists(plugin.getDataFolder() + "/config.yml")) {
+			// save the file
+			plugin.info("configuration did not exist, creating default..");
+			// create the file
+			new File(plugin.getDataFolder().toString()).mkdir();
+			if(!save()) {
+				// if the save failed,
+				// get out of here!
+				plugin.error("failed to save the configuration!");
+				return false;
+			}
+		}
 		// TODO: load the config!
+		return true;
+	}
+
+	public Boolean save() {
+		plugin.info("saving configuration...");
+		// open the file
+		File configFile = new File(plugin.getDataFolder() + "/config.yml");
+		try {
+			if(!configFile.exists()) {
+				// make the file if it doesn't exist
+				configFile.createNewFile();
+			}
+			// now write out to it
+			FileWriter out = new FileWriter(plugin.getDataFolder() + "/config.yml");
+
+			// the header
+			out.write("---\r\n");
+
+			// and close up shop
+			out.close();
+			configFile.close();
+		}
+		catch(IOException ex) {
+			// something went wrong!
+			log.error("something went wrong when saving the configuration!");
+			return false;
+		}
+		return true;
 	}
 
 	// create a "class" in here to store config options!
