4768e5798e3b1a908df17d36934bc6e30189d7fe
Kenton Hamaluik <kenton@hamaluik.com>
2012-02-12 13:20:21 -0700

    removed binaries

--- com/mcnsa/mcnsachat2/commands/CommandChannel.java
+++
@@ -1,9 +0,0 @@
-package com.mcnsa.mcnsachat2.commands;
-
-public Class CommandChannel implements Command {
-	public boolean onCommand(CommandSender sender, String[] args){}
-	public String requiredPermission(){}
-	public String getCommand(){}
-	public String getArguments(){}
-	public String getDescription(){}
-}
